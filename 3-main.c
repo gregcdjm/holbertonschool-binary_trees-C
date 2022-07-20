b93f28e3dc1a1b22c88b5a3020cc179d29899b38
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-20 10:16:59 +0200

    e

--- 3-main.c
+++ 3-main.c
@@ -15,9 +15,9 @@ int main(void)
     root = binary_tree_node(NULL, 0);
     root->left = binary_tree_node(root, 1);
     root->right = binary_tree_node(root, 2);
-    binary_tree_insert_right(root, 4);
+    binary_tree_insert_right(root->left, 4);
     binary_tree_insert_left(root->left, 3);
-    binary_tree_insert_right(root, 6);
+    binary_tree_insert_right(root->right, 6);
     binary_tree_insert_left(root->right, 5);
     printf("original : \n\n");
     binary_tree_print(root);
