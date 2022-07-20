ca6f9fd9ec5a311cfc0b8aa6b68fcf065ce4a858
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-20 17:13:37 +0200

    betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,9 +8,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        if(tree == 0)
-		return 0;
-        else
-		return (1+ binary_tree_size(tree->left)
+	if (tree == 0)
+		return (0);
+	else
+		return (1 + binary_tree_size(tree->left)
 			+ binary_tree_size(tree->right));
 }
