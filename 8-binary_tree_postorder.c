1873873116d4584882b78df8287c8d83329240ac
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-20 14:24:53 +0200

    f

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,17 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - goes throught the binary tree using pre-order
+ * binary_tree_postorder - goes throught the binary tree using post-order
  *
- * @tree: tree to delete
+ * @tree: to go
+ * @func: func to print n
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree)
 	{
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
 		func(tree->n);
 	}
 }
