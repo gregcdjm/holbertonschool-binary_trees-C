74e822d54a2adb6aef590b78a95091ecf014aa40
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-20 14:20:53 +0200

    8

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - goes throught the binary tree using post-order
+ *
+ * @tree: to go
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
