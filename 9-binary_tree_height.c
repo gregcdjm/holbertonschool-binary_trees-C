ef6d49e72954bcc44564f0054420ca88ca61a0bb
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-20 16:38:12 +0200

    9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - how height ?
+ *
+ * @tree: the tree
+ * Return: the height of the tree otherwise 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t len_g = 0, len_d = 0;
+	if (!tree)
+		return (0);
+	if (tree->left)
+		len_g = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		len_d = binary_tree_height(tree->right) + 1;
+	if (lend_d > lend_g)
+		return (lend_d);
+	return (len_g);
+}
