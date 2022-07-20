60a091aae922abd8539de33c5bcf8afae1925d7f
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-20 16:05:38 +0200

    d

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,8 +11,10 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		binary_tree_postorder(tree->left, func);
 		func(tree->n);
-		binary_tree_postorder(tree->right, func);
+		if (tree->left)
+			binary_tree_preorder(tree->left, func);
+		if (tree->right)
+			binary_tree_preorder(tree->right, func);
 	}
 }
