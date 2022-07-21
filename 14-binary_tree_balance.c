d8860f72a8c73d815b10b447d22f850331d185a5
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-21 10:09:44 +0200

    d

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - find the height of the tree
+ * binary_tree_heigh - find the height of the tree
  * @tree: binary tree
  * Return: 0 or the height of the tree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_heigh(const binary_tree_t *tree)
 {
 	size_t height_l = 0, height_r = 0;
 
@@ -13,9 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		height_l = binary_tree_height(tree->left) + 1;
+		height_l = binary_tree_heigh(tree->left) + 1;
 	if (tree->right)
-		height_r = binary_tree_height(tree->right) + 1;
+		height_r = binary_tree_heigh(tree->right) + 1;
 
 	if (height_l > height_r)
 		return (height_l);
@@ -38,9 +38,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		height_l = binary_tree_height(tree->left) + 1;
+		height_l = binary_tree_heigh(tree->left) + 1;
 	if (tree->right)
-		height_r = binary_tree_height(tree->right) + 1;
+		height_r = binary_tree_heigh(tree->right) + 1;
 
 	return (height_l - height_r);
 }
--- WTF_it_do_not_work/14-binary_tree_balance.c
+++ WTF_it_do_not_work/14-binary_tree_balance.c
@@ -1,4 +1,4 @@
-#include "binary_trees.h"
+#include "../binary_trees.h"
 
 /**
  * binary_tree_heig - how height ?
